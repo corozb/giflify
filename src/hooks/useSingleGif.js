062b0da14c8c6264704c3e8577c313d69ed958f6
Cristian Orozco <cristianorozco@gmail.com>
2020-12-28 12:56:31 -0500

    Loading & Error - Spinner loading - Manage Error and 404 page

--- src/hooks/useSingleGif.js
+++ src/hooks/useSingleGif.js
@@ -8,12 +8,23 @@ export default function useSingleGif(id) {
   const gifFromCache = gifs.find((singleGif) => singleGif.id === id)
 
   const [gif, setGif] = useState(gifFromCache)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
 
   useEffect(() => {
     if (!gif) {
-      getSingleGif({ id }).then(setGif)
+      setIsLoading(true)
+      getSingleGif({ id })
+        .then((gif) => {
+          setGif(gif)
+          setIsLoading(false)
+        })
+        .catch((error) => {
+          setIsLoading(false)
+          setIsError(true)
+        })
     }
   }, [gif, id])
 
-  return { gif }
+  return { gif, isLoading }
 }
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -2,17 +2,22 @@ import React from 'react'
 
 import Gif from 'components/Gif/Gif'
 import useSingleGif from 'hooks/useSingleGif'
+import Spinner from 'components/Spinner/Spinner'
+import { Redirect } from 'wouter'
 
 const Detail = ({ params }) => {
   const { id } = params
-  const { gif } = useSingleGif(id)
+  const { gif, isLoading, isError } = useSingleGif(id)
+
+  if (isLoading) return <Spinner />
+  if (isError) return <Redirect to='/404' />
   if (!gif) return null
 
   return (
-    <>
+    <Redirect>
       <h3 className='App-title'>{gif.title}</h3>
       <Gif {...gif} />
-    </>
+    </Redirect>
   )
 }
 
